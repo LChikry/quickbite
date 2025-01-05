9048a65256d8b3fee066406a7c27b6619109ca01
LChikry <LChikry@gmail.com>
2025-01-05 23:31:10 +0100

    introducing MessageFactory

--- src/main/java/org/quickbitehub/communicator/MessageFactory.java
+++ src/main/java/org/quickbitehub/communicator/MessageFactory.java
@@ -1,4 +1,9 @@
 package org.quickbitehub.communicator;
 
 public class MessageFactory {
+
+	public static void sendIncorrectOperationNotice(Long telegramId) {
+		String message = Emoji.ORANGE_CIRCLE.getCode() + " You cannot perform this operation\\, please follow the standard procedures and operations\\.";
+		MessageHandler.sendText(telegramId, message, SHORT_DELAY_TIME_SEC);
+	}
 }
